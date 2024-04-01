e3f0467c6f75448fd907e69fb683af2636181cc6
Yan Dvoretskiy <yandvo@gmail.com>
2024-04-01 15:03:57 -0400

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- User Files
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+
+-- ColorScheme
+vim.cmd "colorscheme moonfly"
